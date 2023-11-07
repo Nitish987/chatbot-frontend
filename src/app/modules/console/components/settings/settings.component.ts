1887475f779cadddae5f069f5a2b97fc764aa04b
Nitish Kumar <nk6655816.nk@gmail.com>
2023-11-07 10:27:45 +0530

    password change ui

--- src/app/modules/console/components/settings/settings.component.ts
+++ src/app/modules/console/components/settings/settings.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserIdentityService } from 'src/app/services/user-identity/user-identity.service';
+import { SettingsService } from '../../services/settings/settings.service';
+import { timer } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthorizationService } from 'src/app/services/auth/authorization.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,7 +12,15 @@ import { UserIdentityService } from 'src/app/services/user-identity/user-identit
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
-  constructor(private identityService: UserIdentityService) {
+  passwordForm = new FormGroup({
+    currentPass: new FormControl('', [Validators.required]),
+    newPass: new FormControl('', [Validators.required]),
+    confirmNewPass: new FormControl('', [Validators.required])
+  });
+  error: string | null = null;
+  success: string | null = null;
+
+  constructor(private identityService: UserIdentityService, private settingsService: SettingsService, private router: Router, private authorizationService: AuthorizationService) {
     this.identityService.getVerified().subscribe(isVerified => {
       if (isVerified) {
         this.identityService.cancel();
@@ -18,5 +31,37 @@ export class SettingsComponent {
 
   iWantToChangeMyEmail() {
     this.identityService.initiate();
+  }
+
+  changePassword() {
+    this.error = null;
+    this.success = null;
+    if (!this.passwordForm.valid) {
+      this.error = "Password fields are empty."
+      return;
+    }
+    if (this.passwordForm.value.newPass !== this.passwordForm.value.confirmNewPass) {
+      this.error = "Password doesn't match."
+      return;
+    }
+
+    this.settingsService.changePassword({
+      currentPassword: this.passwordForm.value.currentPass!,
+      newPassword: this.passwordForm.value.confirmNewPass!
+    }).subscribe(res => {
+      if (res.success()) {
+        this.success = res.data()['message'];
+        timer(2000).subscribe(millis => {
+          this.router.navigateByUrl('/auth/login');
+        });
+      } else {
+        this.error = res.error();
+      }
+    });
+  }
+
+  onForgetPassword() {
+    this.authorizationService.deleteClientAuthData();
+    this.router.navigateByUrl('/auth/recovery');
   }
 }
--- src/app/modules/console/services/settings/settings.service.ts
+++ src/app/modules/console/services/settings/settings.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+import { AuthorizationService } from 'src/app/services/auth/authorization.service';
 import { HttpService } from 'src/app/services/http/http.service';
 import { ResponseCollector } from 'src/app/utils/response-collector';
 
@@ -8,7 +9,7 @@ import { ResponseCollector } from 'src/app/utils/response-collector';
 })
 export class SettingsService {
 
-  constructor(private http: HttpService) { }
+  constructor(private http: HttpService, private authorization: AuthorizationService) { }
 
   emailChange(email: string) {
     return this.http.post('/account/v1/account-email-change/', {email}).pipe(map(res => {
@@ -23,6 +24,20 @@ export class SettingsService {
   emailChangeVerification(otp: string) {
     return this.http.post('/account/v1/account-email-change-verify/', {otp}).pipe(map(res => {
       try {
+        return new ResponseCollector(res);
+      } catch(e) {
+        return ResponseCollector.localErrorResponse();
+      }
+    }));
+  }
+
+  changePassword({currentPassword, newPassword}: {currentPassword: string, newPassword: string}) {
+    return this.http.post('/account/v1/account-password-change/',{
+      password: currentPassword,
+      new_password: newPassword
+    }).pipe(map(res => {
+      try {
+        this.authorization.deleteClientAuthData();
         return new ResponseCollector(res);
       } catch(e) {
         return ResponseCollector.localErrorResponse();
